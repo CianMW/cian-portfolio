cbc42a5cc9cb3d0cc7c4e2ad671e2d2b3cdbeca9
CianMW <88888516+CianMW@users.noreply.github.com>
2022-06-17 16:22:00 +0100

    engine, bakery, cv update
    - updated engine info in package json
    - added bakery link in commercial section
    - replaced old cv with new version

--- src/components/corporate/CommerceWorkContainer.jsx
+++ src/components/corporate/CommerceWorkContainer.jsx
@@ -9,10 +9,19 @@ return(
         <Row className="t-cont">
         <h3 className="text-center">Bellow you'll find some sample sites with a mixture of styles and designs</h3>
         </Row>
-        <Row>
-            <Col>
+        <Row className="mt-3">
+            <Col sm={6} className="text-center">
+                <h2>Loaven Bakery</h2>
+           <p>A single page site for a bakery, including a responsive sidebar, interactive menu and a contact form.</p>
+            </Col>
+            <Col sm={3}>
             <Link to="/sampleBakery">
-            <Button>Bakery</Button>
+            <img src="https://i.ibb.co/W5V1Ryr/webpage-Screenshot.jpg" alt="bakery logo" style={{width:"100%"}}/>
+            </Link>
+            </Col>
+            <Col sm={3} className="mt-md-0 mt-3">
+            <Link to="/sampleBakery">
+            <img src="https://i.ibb.co/rkVk1RW/website-Screenshot2.jpg" alt="bakery logo" style={{width:"100%"}}/>
             </Link>
             </Col>
         </Row>
